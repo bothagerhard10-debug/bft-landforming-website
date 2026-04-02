a782a92eb645293a8ccf610eb0752c73e84110cb
bothagerhard10-debug <bothagerhard10@gmail.com>
2026-04-02 22:31:03 +0200

    Configure Firebase project deployment

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_FIREBASE_APP_ID"
+  apiKey: "AIzaSyDiGdHu29wyMYhVUTf4oITNoBr-1eJ2BvM",
+  authDomain: "jou-plaas-c3bd1.firebaseapp.com",
+  projectId: "jou-plaas-c3bd1",
+  storageBucket: "jou-plaas-c3bd1.firebasestorage.app",
+  messagingSenderId: "865732875739",
+  appId: "1:865732875739:web:c68d84d5d49df155c343fe",
+  measurementId: "G-YG5ET77EMW"
 };
 
 export const firebaseCollections = {
